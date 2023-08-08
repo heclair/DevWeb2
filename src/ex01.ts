be55f5db27f6721d32e158ba4d599c40a28bd874
heclair <heclair@hotmail.com>
2023-08-07 22:26:35 -0300

    primeiro committ

--- src/ex01.ts
+++ src/ex01.ts
@@ -10,3 +10,10 @@ console.log(formatar("12"));
 const nomes = ["Ana","Pedro","Lucas","Maria"];
 
 const cada = nomes.forEach((nomes)=>console.log(`<div>${nomes}</div>`))
+
+//EX 003
+
+const carros = ["Gol","Corsa","Uno","Fiesta"];
+const motos = ["CG", "XRE", "Biz"];
+const veiculos = carros.forEach((carros)=>console.log(`<div>${carros}</div>`));
+const motocicleta = motos.forEach((motos)=>console.log(`<div>${motos}</div>`));
