03074c55194b08de8cb1cb15bf4bfaf07b90a747
heclair <heclair@hotmail.com>
2023-08-07 22:20:08 -0300

    primeiro committ

--- src/ex01.ts
+++ src/ex01.ts
@@ -1,4 +1,12 @@
+//EX 001
+
 const formatar = (entrada: string):string =>`<div>${entrada}</div>`; 
 
 console.log(formatar("ana"));
 console.log(formatar("12"));
+
+// EX 002
+
+const nomes = ["Ana","Pedro","Lucas","Maria"];
+
+const cada = nomes.forEach((nomes)=>console.log(`<div>${nomes}</div>`))
