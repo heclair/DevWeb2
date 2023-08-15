51e51b61352722f6edcf7729c17f5898680878f3
heclair <heclair@hotmail.com>
2023-08-14 21:31:23 -0300

    primeiro commit

--- src/AULA01/ex04.ts
+++ src/AULA01/ex04.ts
@@ -4,12 +4,14 @@ function item(entrada: string): string {
   return `\t<li>${entrada}</li>\n`;
 }
 function listar(elementos: string[]): string {
-  let soma = "";
-  for (let i = 0; i < elementos.length; i++) {
-    soma += item(elementos[i]);
-  }
-  return `<ul>\n${soma}</ul>\n`;
+  const lista2 = elementos.reduce(
+    (acumulador, elemento) => acumulador + item(elemento),
+    ""
+  );
+  return `<ul>\n${lista2}</ul>\n`;
 }
 const frutas = ["Manga", "Laranja", "Maça", "Uva"];
 const resultado = listar(frutas);
-console.log(resultado);
+//console.log(resultado); TIRAR COMENTÁRIO P/ USAR A FUNÇÃO
+
+export default listar
---
+++ src/AULA01/ex05.ts
@@ -0,0 +1,6 @@
+import listar from "./ex04";
+
+const legumes = "Beterraba,Cenoura,Tomate,Repolho";
+const legumesArray = legumes.split(",");
+const resultado1 = listar(legumesArray);
+console.log(resultado1);
