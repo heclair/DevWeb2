490722d824333dd7970b0b996ddd28212816eb74
heclair <heclair@hotmail.com>
2023-08-21 21:51:36 -0300

    aula3

--- src/AULA01/ex10.ts
+++ src/AULA01/ex10.ts
@@ -1,6 +1,12 @@
-import axios from 'axios'
+import axios from "axios";
 
 const url = "http://servicodados.ibge.gov.br/api/v1/localidades/estados";
-axios.get(url)
-.then( ({data}) => console.log(data) )
-.catch( e => console.log(e.message ));
+axios
+  .get(url)
+  .then(({ data }) => {
+    data.forEach((elemento:any) => {
+      console.log(elemento.nome,"-" ,elemento.sigla);
+      
+    });
+  })
+  .catch((e) => console.log(e.message));
